f8870457e30107d833de04431f3b0c051932efed
Giau Ngo <giaunv@gmail.com>
2015-04-19 18:18:51 +0700

    Project Setup

---
+++ spaceshooter-spritekit-objc-iphone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  spaceshooter-spritekit-objc-iphone
+//
+//  Created by giaunv on 4/19/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ spaceshooter-spritekit-objc-iphoneTests/spaceshooter_spritekit_objc_iphoneTests.m
@@ -0,0 +1,40 @@
+//
+//  spaceshooter_spritekit_objc_iphoneTests.m
+//  spaceshooter-spritekit-objc-iphoneTests
+//
+//  Created by giaunv on 4/19/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface spaceshooter_spritekit_objc_iphoneTests : XCTestCase
+
+@end
+
+@implementation spaceshooter_spritekit_objc_iphoneTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
